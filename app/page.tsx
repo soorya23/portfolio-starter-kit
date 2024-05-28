6e86a094eef0204bb09e6643b2d85c1937244345
soorya23 <33139762+soorya23@users.noreply.github.com>
2024-05-28 15:25:30 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ export default function Page() {
         {`Hi! My name is Soorya Sundravel. I am a Computer Science major from the University of Minnesota. I am passionate about leveraging data and AI to build software applications that help people's lives.
         With a strong foundation in programming and problem-solving, I thrive on tackling complex challenges and turning innovative ideas into reality.`}
       </p>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter"></h1>
+      <h2 className="mb-8 text-2xl font-semibold tracking-tighter">
       Experience
-      </h1>
+      </h2>
       <p className=mb-4">
         {Verizon, Accenture, AP Stamp}
     </p>
